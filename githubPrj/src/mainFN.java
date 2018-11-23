b896f8989e410e16c27db30737d4cf4a0665b2f0
minda <minda@10.64.144.12>
2018-11-23 11:16:12 +0900

    permutation function added for decryption of transposition cipher

--- githubPrj/src/mainFN.java
+++ githubPrj/src/mainFN.java
@@ -498,4 +498,31 @@ public class mainFN {
 			}
 		}
 	}
+	
+	//From this part, they are for permutation.
+	public static void perm(int[] arr, int pivot) { 
+		if(pivot == arr.length) { 
+			print(arr); return; 
+		} 
+		for(int i = pivot; i < arr.length; i++) { 
+			swap(arr, i, pivot); 
+			perm(arr, pivot + 1); 
+			swap(arr, i, pivot); 
+		} 
+	} 
+
+	public static void swap(int[] arr, int i, int j) { 
+		int temp = arr[i]; 
+		arr[i] = arr[j]; 
+		arr[j] = temp; 
+	} 
+
+	public static void print(int[] arr) { 
+		for(int i = 0; i < arr.length; i++) { 
+			if(i == arr.length - 1) 
+				System.out.println(arr[i]); 
+			else 
+				System.out.print(arr[i]); 
+		} 
+	} 
 }
